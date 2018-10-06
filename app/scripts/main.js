75501f165e48e63deae6d0533ff0ae0bd33c49b6
taise <taise515@gmail.com>
2018-10-06 20:38:44 +0900

    :sparkles: Register a Service Worker

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -42,3 +42,20 @@ function urlB64ToUint8Array(base64String) {
   }
   return outputArray;
 }
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
