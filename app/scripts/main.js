cff48162d36cfbb769671b69542a9042f1ef7151
taise <taise515@gmail.com>
2018-10-06 21:13:48 +0900

    disablable subscribe

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -61,6 +61,13 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 }
 
 function updateBtn() {
+  if (Notification.permission === 'denied') {
+    pushButton.textContent = 'Push Messaging Blocked.';
+    pushButton.disabled = true;
+    updateSubscriptionOnServer(null);
+    return;
+  }
+
   if (isSubscribed) {
     pushButton.textContent = 'Disable Push Messaging';
   } else {
