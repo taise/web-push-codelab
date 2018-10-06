b6dcecb654ec1326a54a9a93c4003444d286295d
taise <taise515@gmail.com>
2018-10-06 21:35:25 +0900

    :+1: unsubscribeUser

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -113,11 +113,31 @@ function updateSubscriptionOnServer(subscription) {
   }
 }
 
+function unsubscribeUser() {
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    if (subscription) {
+      return subscription.unsubscribe();
+    }
+  })
+  .catch(function(error) {
+    console.log('Error unsubscribing', error);
+  })
+  .then(function() {
+    updateSubscriptionOnServer(null);
+
+    console.log('User is unsubscribed.');
+    isSubscribed = false;
+
+    updateBtn();
+  });
+}
+
 function initializeUI() {
   pushButton.addEventListener('click', function() {
     pushButton.disabled = true;
     if (isSubscribed) {
-      // TODO: Unsubscribe user
+      unsubscribeUser();
     } else {
       subscribeUser();
     }
