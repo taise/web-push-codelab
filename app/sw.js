b9248193a6f940db4df38fd775bf1d70039c0e92
taise <taise515@gmail.com>
2018-10-06 21:33:20 +0900

    :+1: add notificationclick

--- app/sw.js
+++ app/sw.js
@@ -22,15 +22,26 @@
 'use strict';
 
 self.addEventListener('push', function(event) {
+  const body = event.data.text();
   console.log('[Service Worker] Push Received.');
-  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+  console.log(`[Service Worker] Push had this data: "${body}"`);
 
   const title = 'Push Codelab';
   const options = {
-    body: 'Yay it works.',
+    body: body,
     icon: 'images/icon.png',
     badge: 'images/badge.png'
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+  );
 });
